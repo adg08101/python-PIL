b2e18aa24f0e1a9a535bf82a18a4b317efea886a
klol <adg08101@gmail.com>
2021-04-16 14:11:29 -0400

    Python PIL
    Auto image reducer

--- pil.py
+++ pil.py
@@ -1,3 +1,18 @@
 from PIL import Image
+import os
 
-img = Image.open('carat diamante DAN_7648 60x1m_Limpiar 2 y embellecer.jpg')
+def main(p):
+    percent = p / 100
+    source = 'source'
+    store = 'store'
+    dir = os.listdir(source)
+    for i in dir:
+        if i.endswith('.jpg'):
+            dir = source + '\\' + i
+            img = Image.open(dir)
+            new_img = img.resize((int(img.size[0] * percent), int(img.size[1] * percent)))
+            new_img.save(store + '\\' + i + '(' + str(int(img.size[0] * percent)) + ', ' +
+                         str(int(img.size[1] * percent)) + ').png', 'png')
+
+if __name__ == '__main__':
+    main(15)
