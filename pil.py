f388cf88df27a485acfdc4842d0c6f4492bd751d
klol <adg08101@gmail.com>
2021-04-16 14:38:49 -0400

    Python PIL
    Code improvements

--- pil.py
+++ pil.py
@@ -5,16 +5,19 @@ def main(p):
     percent = p / 100
     source = 'source'
     store = 'store'
-    os.mkdir(source)
-    os.mkdir(store)
+    os.makedirs(source, exist_ok=True)
+    os.makedirs(store, exist_ok=True)
     dir = os.listdir(source)
     for i in dir:
+        print('Resizing file ' + str(i))
         if i.endswith('.jpg'):
             dir = source + '\\' + i
             img = Image.open(dir)
             new_img = img.resize((int(img.size[0] * percent), int(img.size[1] * percent)))
-            new_img.save(store + '\\' + i + '(' + str(int(img.size[0] * percent)) + ', ' +
+            new_img.save(store + '\\' + i.replace('.jpg', '') + '_resized_to_(' + str(int(img.size[0] * percent)) + ', ' +
                          str(int(img.size[1] * percent)) + ').png', 'png')
+    print('All work done... press any key to exit.')
+    input()
 
 if __name__ == '__main__':
     main(15)
